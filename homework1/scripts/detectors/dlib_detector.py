af01b44cd48d5622ee2757bcb11f4f774870100d
drobilc <drobilc@github.com>
2020-03-28 16:07:40 +0100

    Add a CNN face detector

--- homework1/scripts/detectors/dlib_detector.py
+++ homework1/scripts/detectors/dlib_detector.py
@@ -7,4 +7,20 @@ class HogDetector():
         self.detector = dlib.get_frontal_face_detector()
 
     def find_faces(self, image):
-        return self.detector(image, 0)
+        # We can use dlib face detection to also return detected face probablities
+        # the first argument is the image, the second is the upscale parameter, that
+        # tells dlib how much to upscale image to detect faces and third parameter
+        # is the minimum probability of detected face
+        faces, scores, idx = self.detector.run(image, 0, 0.25)
+        return faces
+
+class CnnDetector():
+
+    def __init__(self, data_path):
+        self.detector = dlib.cnn_face_detection_model_v1(data_path)
+    
+    def find_faces(self, image):
+        detections = self.detector(image, 0)
+        rects = dlib.rectangles()
+        rects.extend([d.rect for d in detections])
+        return rects
--- homework1/scripts/face_detector.py
+++ homework1/scripts/face_detector.py
@@ -19,7 +19,7 @@ from std_msgs.msg import ColorRGBA
 
 # Both detectors can be used to find faces in images
 from detectors.haar_detector import HaarDetector
-from detectors.dlib_detector import HogDetector
+from detectors.dlib_detector import HogDetector, CnnDetector
 
 class FaceFinder(object):
     
@@ -30,17 +30,23 @@ class FaceFinder(object):
         # Read parameters from our face_finder.launch file
         self.display_camera_window = rospy.get_param('~display_camera_window', False)
         self.haar_cascade_data_file_path = rospy.get_param('~haar_cascade_data_file_path', '')
+        self.cnn_face_detector_data_file_path = rospy.get_param('~cnn_face_detector_data_file_path', '')
         self.rotate_image = rospy.get_param('~rotate_image', False)
         
         # An object we use for converting images between ROS format and OpenCV format
         self.bridge = CvBridge()
 
         # We can use dlib or haar detector here, just uncomment correct line
-        self.face_detector = HogDetector()
-        # self.face_detector = HaarDetector(self.haar_cascade_data_file_path)
+        # self.face_detector = HogDetector()
+        # self.face_detector = CnnDetector(self.cnn_face_detector_data_file_path)
+        self.face_detector = HaarDetector(self.haar_cascade_data_file_path)
 
         # Subscriber for new camera images (the video_stream_opencv publishes to different topic)
         self.image_subscriber = rospy.Subscriber('/camera/image_raw', Image, self.image_callback, buff_size=200*1024*1024, queue_size=None, tcp_nodelay=True)
+
+        # How much we should downscale image before trying to find faces
+        # For example - factor 4 means that that the new image width will be width / 4
+        self.downscale_factor = 4
 
     def process_face(self, image, face):
         # Get coordinates of the rectangle around the face
@@ -55,21 +61,28 @@ class FaceFinder(object):
         # Mark the face on our image
         cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 3, 8, 0)
     
+    def preprocess_image(self, image):
+        # Get image width and height to calculate new size
+        height, width, channels = image.shape
+        new_width, new_height = (int(width / self.downscale_factor), int(height / self.downscale_factor))
+
+        # Resize image to be 1/4 the original size, so the face detector is faster
+        image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
+
+        # If the rotate_image argument is set to true, rotate image 90 deg clockwise
+        if self.rotate_image:
+            image = cv2.transpose(image)
+        
+        # Convert the image to rgb for faster image processing?
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        return image
+    
     def image_callback(self, rgb_image_message):
         # This function will be called when new camera rgb image is received
         rospy.loginfo('New image frame received')
         rgb_image = self.bridge.imgmsg_to_cv2(rgb_image_message, "bgr8")
 
-        # Get image width and height to calculate new size
-        height, width, channels = rgb_image.shape
-        new_width, new_height = (int(width / 4), int(height / 4))
-
-        # Resize image to be 1/4 the original size, so the face detector is faster
-        rgb_image = cv2.resize(rgb_image, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
-
-        # If the rotate_image argument is set to true, rotate image 90 deg clockwise
-        if self.rotate_image:
-            rgb_image = cv2.transpose(rgb_image)
+        rgb_image = self.preprocess_image(rgb_image)
 
         face_rectangles = self.face_detector.find_faces(rgb_image)
         rospy.loginfo('Found {} faces'.format(len(face_rectangles)))
