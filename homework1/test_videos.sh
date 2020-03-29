a0b252fcd89636bc4c3e2617b06a87b1f5df4cc0
drobilc <drobilc@github.com>
2020-03-29 10:31:30 +0200

    Update video testing script to test all configurations for all files

--- homework1/test_videos.sh
+++ homework1/test_videos.sh
@@ -1,5 +1,18 @@
-# Timeout in seconds
-TIMEOUT=10
+run_test () {
+    # Function parameters: video, frames, detector, downscale, black_and_white
+    echo "[" $1 "] Testing configuration: detector = " $3 ", downscale = " $4 ", black_and_white = " $5
+    
+    # Run the video publisher, save the process id in P1
+    roslaunch homework1 video_publisher.launch video_source:=$1 frames:=$2 &
+    P1=$!
+    
+    # Run the face detector, save the process id in P2
+    roslaunch homework1 face_detector.launch display_camera_window:=true rotate_image:=true detector:=$3 frames:=$2 downscale:=$4 bw:=$5 &
+    P2=$!
+
+    # Wait for both processes to finish
+    wait $P1 $P2
+}
 
 # Find all files inside current folder, this script must be run from videos folder 
 videos=`ls *.mp4`
@@ -7,15 +20,18 @@ videos=`ls *.mp4`
 for video in $videos
 do
     echo "Testing video: " $video
-
-    # First, run our face_detector, then start video stream
+    
+    # Get number of frames of video (requires ffmpeg to be installed)
     frames=$(ffprobe -select_streams v -show_streams $video 2>/dev/null | grep nb_frames | sed -e 's/nb_frames=//')
     echo "Total number of frames: " $frames
-    timeout 15 roslaunch homework1 video_publisher.launch video_source:=$video frames:=$frames &
-    P1=$!
-    timeout 15 roslaunch homework1 face_detector.launch display_camera_window:=true rotate_image:=true detector:=3 frames:=$frames&
-    P2=$!
 
-    # wait for both processes to finish
-    wait $P1 $P2
+    for detector in 1 2 3
+    do
+        for downscale in 8 4 2 1
+        do
+            run_test $video $frames $detector $downscale false
+            run_test $video $frames $detector $downscale true
+        done
+    done
+
 done
