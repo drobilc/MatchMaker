82511fe538c83ef628dbc2cee2157408aebc6c7a
drobilc <drobilc@github.com>
2020-04-01 21:47:24 +0200

    Add pose forwarding to robustifier

--- love_maker/scripts/robustifier.py
+++ love_maker/scripts/robustifier.py
@@ -12,11 +12,19 @@ class Robustifier(object):
 
         # The face detector publishes faces to /face_detections_raw, so create a
         # subscriber that we will use to read positions
-        self.face_subscriber = rospy.Subscriber('/face_detections_raw', Pose, self.on_face_detection, queue_size=10)
+        self.raw_face_subscriber = rospy.Subscriber('/face_detections_raw', Pose, self.on_face_detection, queue_size=10)
+        
+        # After the robistufier determines face position, it should send a pose
+        # to movement_controller node
+        self.face_publisher = rospy.Publisher('/face_detections', Pose, queue_size=10)
     
     def on_face_detection(self, face_pose):
         # A new face has been detected
         rospy.loginfo('A new face pose received: {}'.format(face_pose))
+
+        # TODO: Only send each face pose once and only send true positive faces
+        # For now, only forward the received face to movement controller
+        self.face_publisher.publish(face_pose)
 
 if __name__ == '__main__':
     robustifier = Robustifier()
