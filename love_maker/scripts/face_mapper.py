4e48624ffbcfae6a6bcf17a30341f147e94cf0b5
drobilc <drobilc@github.com>
2020-04-28 13:41:30 +0200

    Update mapper to calculate approaching point, update robustifier to be more general

--- love_maker/scripts/face_mapper.py
+++ love_maker/scripts/face_mapper.py
@@ -15,7 +15,100 @@ from visualization_msgs.msg import Marker, MarkerArray
 from image_geometry import PinholeCameraModel
 from geometry_msgs.msg import Point, Vector3
 
+import math
+import numpy as np
+
+from geometry_msgs.msg import Pose, PoseStamped, Vector3, Point, Quaternion
+from visualization_msgs.msg import MarkerArray, Marker
+from std_msgs.msg import ColorRGBA
+
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import tf2_ros
+from tf2_geometry_msgs import PointStamped
+
+import utils
+
 class DetectionMapper():
+
+    def __init__(self):
+        rospy.init_node('mapper', anonymous=False)
+
+        self.region_scope = rospy.get_param('~region', 1)
+        self.buffer_size = rospy.get_param('~camera_buffer_size', 50)
+        rospy.wait_for_service('localizer/localize')
+
+        self.camera_infos = collections.deque(maxlen = self.buffer_size)
+        self.detections_sub = message_filters.Subscriber('detections', Detection)
+        self.detections_sub.registerCallback(self.detections_callback)
+
+        self.camera_sub = message_filters.Subscriber('/camera/rgb/camera_info', CameraInfo)
+        self.camera_sub.registerCallback(self.camera_callback)
+
+        self.localize = rospy.ServiceProxy('localizer/localize', Localize)
+
+        self.approaching_point_publisher = rospy.Publisher('/face_detections_raw', PoseStamped, queue_size=100)
+
+        self.markers = MarkerArray()
+        self.markers_publisher = rospy.Publisher('/faces', MarkerArray, queue_size=1000)
+
+        self.tf_buf = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buf)
+    
+    def compute_approaching_point(self, face_point):
+        # Construct face point
+        global_face_point = PointStamped()
+        global_face_point.header.frame_id = face_point.header.frame_id
+        global_face_point.header.stamp = face_point.header.stamp
+        global_face_point.point.x = face_point.pose.position.x
+        global_face_point.point.y = face_point.pose.position.y
+        global_face_point.point.z = face_point.pose.position.z
+
+        # Construct robot point
+        robot_point = PointStamped()
+        robot_point.header.frame_id = "camera_depth_optical_frame"
+        robot_point.header.stamp = face_point.header.stamp
+        robot_point.point.x = 0
+        robot_point.point.y = 0
+        robot_point.point.z = 0
+
+        # Transform face's and robot's position to global coordinates
+        global_face_point = self.tf_buf.transform(global_face_point, "map")
+        global_robot_point = self.tf_buf.transform(robot_point, "map")
+
+        # Calculate orientation vector
+        global_position_face = global_face_point.point
+        global_position_robot = global_robot_point.point
+        orientation_parameters = []
+        orientation_parameters.append(global_position_robot.x - global_position_face.x)
+        orientation_parameters.append(global_position_robot.y - global_position_face.y)
+        orientation_parameters.append(0.002472)
+
+        # Calculate orientation
+        orientation = math.atan2(orientation_parameters[1], orientation_parameters[0])
+        orientation_quaternion = quaternion_from_euler(0, 0, orientation + math.pi)
+        orientation_quaternion = Quaternion(orientation_quaternion[0], orientation_quaternion[1], orientation_quaternion[2], orientation_quaternion[3])
+
+        # Normalize orientation vector
+        normalized_orientation = orientation_parameters / np.linalg.norm(orientation_parameters)
+        
+        # Calculate approaching points
+        approaching_point = PointStamped()
+        approaching_point.header = global_face_point.header
+        approaching_point.point.x = global_position_face.x + 0.5 * normalized_orientation[0]
+        approaching_point.point.y = global_position_face.y + 0.5 * normalized_orientation[1]
+        approaching_point.point.z = global_position_face.z + 0.5 * normalized_orientation[2]
+
+        approaching_point.point.y = max(approaching_point.point.y, 1.2)
+
+        # Construct approaching point pose
+        approaching_pose = PoseStamped()
+        approaching_pose.header = global_face_point.header
+        approaching_pose.pose.position.x = approaching_point.point.x
+        approaching_pose.pose.position.y = approaching_point.point.y
+        approaching_pose.pose.position.z = approaching_point.point.z
+        approaching_pose.pose.orientation = orientation_quaternion
+
+        return approaching_pose
 
     def camera_callback(self, camera_info):
         self.camera_infos.append(camera_info)
@@ -46,63 +139,21 @@ class DetectionMapper():
         if not localization:
             return
         
-        #rospy.loginfo(localization)
-        
+        # Construct a PoseStamped at position of the face and publish it
         pose = PoseStamped()
         pose.header.stamp = detection.header.stamp
         pose.header.frame_id = detection.header.frame_id
         pose.pose = localization.pose
-        self.face_publisher.publish(pose)
 
-        """marker = Marker()
-        marker.header.stamp = detection.header.stamp
-        marker.header.frame_id = detection.header.frame_id
-        marker.pose = localization.pose
-        marker.type = Marker.CUBE
-        marker.action = Marker.ADD
-        marker.frame_locked = False
-        marker.lifetime = rospy.Duration.from_sec(1)
-        marker.id = self.marker_id_counter
-        marker.scale = Vector3(0.1, 0.1, 0.1)
-        marker.color = ColorRGBA(1, 0, 0, 1)
-        self.markers.markers.append(marker)
-        self.marker_id_counter += 1"""
+        face_marker = utils.stamped_pose_to_marker(pose, index=len(self.markers.markers), color=ColorRGBA(1, 0, 1, 1))
+        self.markers.markers.append(face_marker)
+        self.markers_publisher.publish(self.markers)
 
-    def flush(self):
-        if not self.markers.markers:
-            self.markers = MarkerArray()
-            return
-        self.markers_pub.publish(self.markers)
-        #self.markers = MarkerArray()
-
-    def __init__(self):
-        self.markers = MarkerArray()
-        self.marker_id_counter = 0
-        self.region_scope = rospy.get_param('~region', 1)
-        self.buffer_size = rospy.get_param('~camera_buffer_size', 50)
-        rospy.wait_for_service('localizer/localize')
-
-        self.camera_infos = collections.deque(maxlen = self.buffer_size)
-        self.detections_sub = message_filters.Subscriber('detections', Detection)
-        self.detections_sub.registerCallback(self.detections_callback)
-
-        self.camera_sub = message_filters.Subscriber('/camera/rgb/camera_info', CameraInfo)
-        self.camera_sub.registerCallback(self.camera_callback)
-
-        self.localize = rospy.ServiceProxy('localizer/localize', Localize)
-        self.markers_pub = rospy.Publisher('markers', MarkerArray)
-
-        self.face_publisher = rospy.Publisher('/face_detections_raw', PoseStamped, queue_size=1)
+        # Compute where the approaching point for this face is. Then send this
+        # data to robustifier.
+        approaching_point = self.compute_approaching_point(pose)
+        self.approaching_point_publisher.publish(approaching_point)
    
 if __name__ == '__main__':
-
-        rospy.init_node('mapper')
-
-        try:
-            mapper = DetectionMapper()
-            r = rospy.Rate(30)
-            while not rospy.is_shutdown():
-                mapper.flush()
-                r.sleep()
-        except rospy.ROSInterruptException: pass
-
+    mapper = DetectionMapper()
+    rospy.spin()
--- love_maker/scripts/robustifier.py
+++ love_maker/scripts/robustifier.py
@@ -13,6 +13,8 @@ from tf2_geometry_msgs import PointStamped
 
 import numpy as np
 
+import utils
+
 class Detection(object):
     
     def __init__(self, pose, number_of_detections = 0):
@@ -47,181 +49,105 @@ class Detection(object):
 
 class Robustifier(object):
 
+    RAW_MARKER_STYLE = {
+        'marker_type': Marker.SPHERE,
+        'color': ColorRGBA(1, 0, 0, 1)
+    }
+    ROBUSTIFIED_MARKER_STYLE = {
+        'marker_type': Marker.SPHERE,
+        'color': ColorRGBA(0, 1, 0, 1),
+        'scale': Vector3(0.2, 0.2, 0.2)
+    }
+
     def __init__(self):
         # Initialize node, don't allow running multiple nodes of this type
-        rospy.init_node('robustifier', anonymous=False)
+        rospy.init_node('robustifier', anonymous=True)
 
         # This parameter tells us how far two face detections have to be to be
         # considered as different positions
-        self.maximum_distance = 0.5
+        self.maximum_distance = rospy.get_param('~maximum_distance', 0.5)
 
         # This parameter tells us how many detections is needed to proclaim a
         # face detection true positive
-        self.minimum_detections = 7
+        self.minimum_detections = rospy.get_param('~minimum_detections', 7)
 
-        # The face detector publishes faces to /face_detections_raw, so create a
-        # subscriber that we will use to read positions
-        self.raw_face_subscriber = rospy.Subscriber('/face_detections_raw', PoseStamped, self.on_face_detection, queue_size=10)
-        
-        # After the robistufier determines face position, it should send a pose
-        # to movement_controller node
-        self.face_publisher = rospy.Publisher('/face_detections', PoseStamped, queue_size=10)
+        # Raw sensor data is read from raw_detection_topic topic, then
+        # robustified and the detections are then sent to detection_topic topic
+        self.raw_detection_topic = rospy.get_param('~raw_detection_topic', '/face_detections_raw')
+        self.detection_topic = rospy.get_param('~detection_topic', '/face_detections')
+        self.marker_topic = rospy.get_param('~marker_topic', '/face_markers')
+
+        # Subscriber and publisher for object detections
+        self.raw_object_subscriber = rospy.Subscriber(self.raw_detection_topic, PoseStamped, self.on_object_detection, queue_size=10)
+        self.object_publisher = rospy.Publisher(self.detection_topic, PoseStamped, queue_size=10)
+
+        # Publisher for publishing raw face detections
+        self.markers_publisher = rospy.Publisher(self.marker_topic, MarkerArray, queue_size=1000)
+        self.markers = MarkerArray()
 
         # TODO: Instead of using a list use a quadtree or a grid
         # A list of Detection objects used for finding the closest pose
-        self.face_detections = []
-
-        # TODO: Define appropriate structures for storing the received poses and
-        self.markers_publisher = rospy.Publisher('face_markers_raw', MarkerArray, queue_size=1000)
-        self.marker_array = MarkerArray()
-        self.marker_number = 1
+        self.object_detections = []
 
         # Object we use for transforming between coordinate frames
         self.tf_buf = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buf)
-    
-    def construct_marker(self, detection, color, scale=0.1):
-        self.marker_number += 1
-        marker = Marker()
-        marker.header.stamp = detection.header.stamp
-        marker.header.frame_id = detection.header.frame_id # camera_depth_optical_frame
-        marker.pose = detection.pose
-        marker.type = Marker.CUBE
-        marker.action = Marker.ADD
-        marker.frame_locked = False
-        marker.lifetime = rospy.Duration.from_sec(10)
-        marker.id = self.marker_number
-        marker.scale = Vector3(scale, scale, scale)
-        marker.color = color
-        
-        return marker
 
-    def already_detected(self, face_pose):
-        for detection in self.face_detections:
-            distance = detection.distance_to(face_pose)
+    def publish_marker(self, pose, marker_style={}):
+        # Construct a marker from detected object pose, add it to markers array
+        # and publish it to marker_topic
+        new_marker = utils.stamped_pose_to_marker(pose, index=len(self.markers.markers), **marker_style)
+        self.markers.markers.append(new_marker)
+        self.markers_publisher.publish(self.markers)
+
+    def already_detected(self, pose_stamped):
+        for detection in self.object_detections:
+            distance = detection.distance_to(pose_stamped)
             if distance <= self.maximum_distance:
-                detection.stamped_pose.header.stamp = face_pose.header.stamp # one solution to "locatioin too far in history"
+                # one solution to "location too far in history"
+                detection.stamped_pose.header.stamp = pose_stamped.header.stamp
                 return detection
     
-    def publish_marker(self, pose, color=ColorRGBA(1, 0, 0, 1), scale=0.1):
-        # Send a list of all markers to visualize data
-        marker = self.construct_marker(pose, color, scale)
-        self.marker_array.markers.append(marker)
-        self.markers_publisher.publish(self.marker_array)
+    def on_object_detection(self, object_pose):
+        # A new object has been detected, robustify it
 
-    def convert_to_approaching_point(self, face_point):
-        # Construct face point
-        global_face_point = PointStamped()
-        global_face_point.header.frame_id = face_point.header.frame_id
-        global_face_point.header.stamp = face_point.header.stamp
-        global_face_point.point.x = face_point.pose.position.x
-        global_face_point.point.y = face_point.pose.position.y
-        global_face_point.point.z = face_point.pose.position.z
-
-        # Construct robot point
-        robot_point = PointStamped()
-        robot_point.header.frame_id = "camera_depth_optical_frame"
-        robot_point.header.stamp = face_point.header.stamp
-        robot_point.point.x = 0
-        robot_point.point.y = 0
-        robot_point.point.z = 0
-
-        # Transform face's and robot's position to global coordinates
-        global_robot_point = self.tf_buf.transform(robot_point, "map")
-
-        # Calculate orientation vector
-        global_position_face = global_face_point.point
-        global_position_robot = global_robot_point.point
-        orientation_parameters = []
-        orientation_parameters.append(global_position_robot.x - global_position_face.x)
-        orientation_parameters.append(global_position_robot.y - global_position_face.y)
-        orientation_parameters.append(0.002472)
-
-        # Calculate orientation
-        orientation = math.atan2(orientation_parameters[1], orientation_parameters[0])
-        orientation_quaternion = quaternion_from_euler(0, 0, orientation + math.pi)
-        orientation_quaternion = Quaternion(orientation_quaternion[0], orientation_quaternion[1], orientation_quaternion[2], orientation_quaternion[3])
-
-        # Normalize orientation vector
-        normalized_orientation = orientation_parameters / np.linalg.norm(orientation_parameters)
-        
-        # Calculate approaching points
-        approaching_point = PointStamped()
-        approaching_point.header = global_face_point.header
-        approaching_point.point.x = global_position_face.x + 0.5 * normalized_orientation[0]
-        approaching_point.point.y = global_position_face.y + 0.5 * normalized_orientation[1]
-        approaching_point.point.z = global_position_face.z + 0.5 * normalized_orientation[2]
-
-        if approaching_point.point.y < 1.2:
-            approaching_point.point.y = 1.2
-
-        # Transform back to frame_id = camera_depth_optical_frames
-        approaching_marker_point = self.tf_buf.transform(approaching_point, "camera_depth_optical_frame")
-
-        # Construct approaching point pose
-        approaching_pose = PoseStamped()
-        approaching_pose.header = global_face_point.header
-        approaching_pose.pose.position.x = approaching_point.point.x
-        approaching_pose.pose.position.y = approaching_point.point.y
-        approaching_pose.pose.position.z = approaching_point.point.z
-        approaching_pose.pose.orientation = orientation_quaternion
-
-        # Construct approaching point marker pose
-        approaching_marker = PoseStamped()
-        approaching_marker.header = approaching_marker_point.header
-        approaching_marker.pose.position.x = approaching_marker_point.point.x
-        approaching_marker.pose.position.y = approaching_marker_point.point.y
-        approaching_marker.pose.position.z = approaching_marker_point.point.z
-
-        return approaching_pose, approaching_marker
-    
-    def on_face_detection(self, face_pose):
-        # A new face has been detected, robustify it
-
-        # First convert the face position to map coordinate frame, so we can
-        # check if the distance to the nearest face is less than half a metre If
+        # First convert the object position to map coordinate frame, so we can
+        # check if the distance to the nearest object is less than half a metre If
         # this is not done, the robustifier tries to compute distances between
         # points in camera_depth_optical_frame frame, which has a different
         # scale than map
-        face_pose = self.tf_buf.transform(face_pose, "map") # red
+        object_pose = self.tf_buf.transform(object_pose, "map")
 
-        self.publish_marker(face_pose)
+        self.publish_marker(object_pose, Robustifier.RAW_MARKER_STYLE)
 
-        # Check if detected face is already in face_detections. This cannot be
+        # Check if detected object is already in object_detections. This cannot be
         # done with simple indexof function because the coordinates will not
         # exactly match
-        saved_pose = self.already_detected(face_pose)
+        saved_pose = self.already_detected(object_pose)
         if saved_pose is not None:
-            rospy.loginfo('Face was already detected, it has been detected {} times'.format(saved_pose.number_of_detections))
+            rospy.loginfo('Object was already detected, it has been detected {} times'.format(saved_pose.number_of_detections))
             saved_pose.number_of_detections += 1
 
-            # If face was detected more than self.minimum_detections, it is now
+            # If object was detected more than self.minimum_detections, it is now
             # considered true positive, send it to movement controller
             if saved_pose.number_of_detections >= self.minimum_detections:
                 if not saved_pose.already_sent:
+                    rospy.loginfo('Sending object location to movement controller')
 
-                    # Move the point 0.5m infront of the face so the robot doesn't bump into it when approaching
-                    face_to_approach = saved_pose.stamped_pose
-                    approaching_point, approaching_marker = self.convert_to_approaching_point(face_to_approach)
-                    rospy.loginfo('Sending face location to movement controller')
-                    self.face_publisher.publish(approaching_point)
+                    self.publish_marker(saved_pose.stamped_pose, Robustifier.ROBUSTIFIED_MARKER_STYLE)
+                    self.object_publisher.publish(saved_pose.stamped_pose)
 
-                    # publish marker for confirmed true positive
-                    self.publish_marker(saved_pose.stamped_pose, ColorRGBA(0, 1, 0, 1), 0.3) # green
-                    
-                    # publish marker for approaching point
-                    self.publish_marker(approaching_marker, ColorRGBA(0, 0, 1, 1), 0.1) # blue
                     saved_pose.already_sent = True
             else:
                 rospy.loginfo('Detection has not yet surpassed the minimum number of detections needed')
-                # The detected face pose and previously saved pose are very similar,
+                # The detected object pose and previously saved pose are very similar,
                 # calculate mean position of both poses and save data to saved_pose
-                saved_pose.update(face_pose)
+                saved_pose.update(object_pose)
         else:
-            rospy.loginfo('Face is probably new.')
+            rospy.loginfo('Object has not yet been deteted')
             # Construct a new detection object, add it to detections
-            saved_pose = Detection(face_pose)
-            self.face_detections.append(saved_pose)
+            saved_pose = Detection(object_pose)
+            self.object_detections.append(saved_pose)
 
 if __name__ == '__main__':
     robustifier = Robustifier()
