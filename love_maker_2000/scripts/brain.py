a8f01ab9acdd22a945c6dde5c52b23ee63d90239
ajdafrankovic <ajda.frankovic@gmail.com>
2020-05-31 15:52:06 +0200

    Use fine approaching task

--- love_maker_2000/scripts/brain.py
+++ love_maker_2000/scripts/brain.py
@@ -154,8 +154,8 @@ class Brain(object):
                     return FaceDetails(pref.hair_length, pref.hair_color)
 
             rospy.logerr("Speech recognition failed, please provide the preferences manually...")
-            hair_length = raw_input("Hair length: ")
-            hair_color = raw_input("Hair color: ")
+            hair_length = raw_input("Hair length (long or short): ")
+            hair_color = raw_input("Hair color (dark or bright): ")
             rospy.logerr("length: {}, color: {}".format(hair_length, hair_color))
             return FaceDetails(hair_length, hair_color)
         except:
@@ -280,6 +280,7 @@ class Brain(object):
     
     # TODO: throw an imaginary coin into the well or make a wish
     def on_cylinder_approached(self, detection, goal_status, goal_result):
+        rospy.logwarn("Let's find you two a ring now!")
         self.start_finding_ring()
 
     def on_start_finding_ring(self):
@@ -295,8 +296,9 @@ class Brain(object):
     def on_start_approaching_ring(self, ring):
         rospy.loginfo("Approaching {} ring".format(self.favorite_color))
         self.wandering_task.cancel()
-        task = self.movement_controller.approach(ring, callback=self.on_ring_approached)
-        self.movement_controller.add_to_queue(task)
+        get_close_task, pickup_task = self.movement_controller.approach(ring, callback=self.on_ring_approached, fine=True)
+        self.movement_controller.add_to_queue(get_close_task)
+        self.movement_controller.add_to_queue(pickup_task)
     
     # TODO: first grab the ring with the robotic arm
     def on_ring_approached(self, detection, goal_status, goal_result):
